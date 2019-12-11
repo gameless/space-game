644a0cc92157b4fa74c5d24599ee0e3be3467580
jonasmit15 <50601012+jonasmit15@users.noreply.github.com>
2019-12-11 14:44:21 -0500

    Add files via upload

---
+++ Assets/Scripts/Ship_Motion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Ship_Motion : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int force_magnitude = 30;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 ship_position = transform.position;
+            Vector3 mouse_position = Input.mousePosition;
+            Vector3 motion_vector_norm = (mouse_position-ship_position).normalized;
+            Vector3 motion_vector = force_magnitude * motion_vector_norm;
+            rb.AddForce(motion_vector);
+            Debug.Log("Pressed Space");
+        }
+    }
+}
